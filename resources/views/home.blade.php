84c94bd007ce109732d07084e94208414935b940
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-03-16 18:56:25 -0400

    Edited home.blade.php style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,66 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="row">
+        <div 
+            class="col-3 p-5"
+            style="padding: 1em;">
+            <img class="rounded-circle" src="https://img.icons8.com/cute-clipart/128/000000/instagram-new.png"/>
+        </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="col-9 pt-5">
+            <div>
+                <h1>Instagram</h1>
+            </div>
 
-                    You are logged in!
+            <div class="d-flex">
+                <div class="pr-5">
+                    <strong>153 </strong>
+                    posts
+                </div>
+
+                <div class="pr-5">
+                    <strong>23k </strong>
+                    followers
+                </div>
+
+                <div class="pr-5">
+                    <strong>212 </strong>
+                    following
                 </div>
             </div>
+
+            <div class="pt-4 font-weight-bold">
+                Instagram.com
+            </div>
+
+            <div>
+                Sharing moments has never been this fun!
+            </div>
+
+            <div>
+                <a href="https://instagram.com">www.instagram.com</a>
+            </div>
+
         </div>
+
+
+        <div class="row pt-5">
+            <div class="col-4">
+                <img class="w-100" src="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.112.929.929a/s640x640/87342432_635895947238639_7505752153798576814_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=103&_nc_ohc=T9IDuUFd79gAX9pPh73&oh=9ace56c3257c0f17b553ed02f28deff1&oe=5E9AE2AC">
+            </div>
+
+            <div class="col-4">
+                <img class="w-100" src="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.106.921.921a/s640x640/84770545_2609265086061431_6387769783591162166_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=111&_nc_ohc=htmeeAn5B_4AX9GwwhR&oh=2ab2a1daa53300315fa803820811cd16&oe=5E9D70ED">
+            </div>
+
+            <div class="col-4">
+                <img class="w-100" src="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/c100.0.501.501a/89260743_2658694764411607_948672185790002051_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=107&_nc_ohc=7XzmYiXTpCgAX9oJwTd&oh=bb1de6e107080ce9d14bab870692dec9&oe=5EA55E5D">
+            </div>
+
+
+        </div>
+
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Instagram Clone</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -23,8 +23,16 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+                    <div>
+                        <img 
+                            src="https://img.icons8.com/cute-clipart/64/000000/instagram-new.png"
+                            style="height: 45px; border-right: .06em solid lightgrey; padding-right: 1em;"/>
+                    </div>
+
+                    <div class="pl-3">
+                        Instagram Clone
+                    </div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
